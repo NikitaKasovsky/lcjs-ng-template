6f256dac8ed0c97b5bc74616e563b348566d701c
Nikita Kasovsky <nikita@kasovsky.ru>
2021-08-10 17:32:46 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 import { Point } from '@arction/lcjs';
 
 @Component({
@@ -7,16 +10,31 @@ import { Point } from '@arction/lcjs';
   <div>
     <app-chart
       [points]="this.points"
+      [points2]="this.points2"
+      [points3]="this.points3"
+      [points4]="this.points4"
+      [points5]="this.points5"
     ></app-chart>
   </div>`,
   styles: ['div { height: 100vh }']
 })
 
-export class AppComponent {
-  points: Point[] = [
-    { x: 0, y: 0 },
-    { x: 1, y: 7 },
-    { x: 2, y: 3 },
-    { x: 3, y: 10 }
-  ];
+export class AppComponent implements OnInit {
+  points: Point[] = [];
+  points2: Point[] = [];
+  points3: Point[] = [];
+  points4: Point[] = [];
+  points5: Point[] = [];
+
+  public ngOnInit(): void {
+    let visits = 10;
+    for (let i = 1; i < 50000; i++) {
+      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+      this.points.push({x: i, y: visits });
+      this.points2.push({x: i, y: visits * 0.3 });
+      this.points3.push({x: i, y: visits * 1.5 });
+      this.points4.push({x: i, y: visits * 2 });
+      this.points5.push({x: i, y: visits * 0.01 });
+    }
+  }
 }
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, AfterViewInit, OnChanges, OnDestroy } from '@angular/core';
-import { lightningChart, ChartXY, Point } from '@arction/lcjs';
+import {
+  lightningChart,
+  ChartXY,
+  Point,
+  SolidLine,
+  SolidFill,
+  ColorHEX
+} from '@arction/lcjs';
 
 @Component({
   selector: 'app-chart',
@@ -12,6 +19,10 @@ export class ChartComponent implements OnChanges, OnDestroy, AfterViewInit {
   chartId: number;
 
   @Input() points: Point[];
+  @Input() points2: Point[];
+  @Input() points3: Point[];
+  @Input() points4: Point[];
+  @Input() points5: Point[];
 
   constructor() {}
 
@@ -24,13 +35,25 @@ export class ChartComponent implements OnChanges, OnDestroy, AfterViewInit {
     // Create chartXY
     this.chart = lightningChart().ChartXY({container: `${this.chartId}`});
     // Set chart title
-    this.chart.setTitle('Getting Started');
+    this.chart.setTitle('IVLIVE CHARTS');
     // Add line series to the chart
     const lineSeries = this.chart.addLineSeries();
+    const lineSeries2 = this.chart.addLineSeries();
+    const lineSeries3 = this.chart.addLineSeries();
+    const lineSeries4 = this.chart.addLineSeries();
+    const lineSeries5 = this.chart.addLineSeries();
     // Set stroke style of the line
-    lineSeries.setStrokeStyle((style) => style.setThickness(5));
+    lineSeries.setStrokeStyle(new SolidLine({ thickness: 1, fillStyle: new SolidFill({ color: ColorHEX('#F00') }) }));
+    lineSeries2.setStrokeStyle(new SolidLine({ thickness: 10, fillStyle: new SolidFill({ color: ColorHEX('#ffb700') }) }));
+    lineSeries3.setStrokeStyle(new SolidLine({ thickness: 3, fillStyle: new SolidFill({ color: ColorHEX('#4af631') }) }));
+    lineSeries4.setStrokeStyle(new SolidLine({ thickness: 5, fillStyle: new SolidFill({ color: ColorHEX('#1531a1') }) }));
+    lineSeries5.setStrokeStyle(new SolidLine({ thickness: 3, fillStyle: new SolidFill({ color: ColorHEX('#bb3cc5') }) }));
     // Add data point to the line series
     lineSeries.add(this.points);
+    lineSeries2.add(this.points2);
+    lineSeries3.add(this.points3);
+    lineSeries4.add(this.points4);
+    lineSeries5.add(this.points5);
   }
 
   ngOnDestroy() {
